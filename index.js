e16647cd1469c64de9aa5b682b2d81d9f79dbfbb
Jussi Palo <jpalo@users.noreply.github.com>
2021-03-17 17:56:54 +0200

    Update index.js
    Added loadNodeContent implementation to support using this plugin together with transformer plugins such as gatsby-transformer-json.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// no-op-lo-op
+"use strict";
+
+const fs = require(`fs-extra`);
+
+function loadNodeContent(fileNode) {
+  return fs.readFile(fileNode.absolutePath, `utf-8`);
+}
+
+exports.loadNodeContent = loadNodeContent;
